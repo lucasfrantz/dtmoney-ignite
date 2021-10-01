d7193a1ea1c639b754be53d371634201001ef815
Lucas Frantz <maxkronus@gmail.com>
2021-10-01 11:01:27 -0300

    Criado TransactionsContext

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { GlobalStyle } from "./styles/global";
 import Modal from 'react-modal'
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root')
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
     
   function handleOpenNewTransactionModal(){
       setIsNewTransactionModalOpen(true)
@@ -18,13 +20,13 @@ export function App() {
       setIsNewTransactionModalOpen(false)
   }  
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
       <NewTransactionModal 
         isOpen = {isNewTransactionModalOpen} 
         onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle/>
-    </>
+    </TransactionsProvider>
   );
 }
